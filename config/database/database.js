6be2598f386146142c61da9e91837577c5ee1a03
Vivek <vivek.k@daffodilsw.com>
2019-04-23 17:24:07 +0530

    update dbuser

--- config/database/database.js
+++ config/database/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://ManazeServices:manaze#123@ds129454.mlab.com:29454/canteen',
+    database: 'mongodb://canteen:manaze#123@ds129454.mlab.com:29454/canteen',
     // database:'mongodb://localhost:27017/canteen',
     secret:'yoursecret'
 }
